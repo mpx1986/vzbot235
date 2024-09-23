35b3b4d267f93c48bd194bdc93a3c19dde528970
pi <pi@vzbot235-1028e19>
2024-09-23 21:15:46 +0200

    flash.sh modified - 23/09/24 - 21:15:44

--- printer_data/config/flash.sh
+++ printer_data/config/flash.sh
@@ -7,18 +7,6 @@ rpi_flash(){
 	make clean KCONFIG_CONFIG=config.rpi
 	make flash KCONFIG_CONFIG=config.rpi -j4 > /dev/null 2>&1
 }
-
-octopus_flash(){
-	echo -e "\033[1;34m\nStep 2: Cleaning and building Klipper firmware for Octopus.\033[0m"
-	make clean KCONFIG_CONFIG=config.octopus
-	make -s KCONFIG_CONFIG=config.octopus -j4  > /dev/null 2>&1
-	mv ~/klipper/out/klipper.bin octopus_klipper.bin
-	echo -e "\033[1;34m\nStep 3: Flashing Klipper to Octopus.\033[0m"
-	python3 ~/katapult/scripts/flashtool.py -i can0 -u 59469bda118c -r
-	sleep 5
-	python3 ~/katapult/scripts/flash_can.py -f ~/klipper/octopus_klipper.bin -d /dev/serial/by-id/stm32h723xx_32001F000F51313531383332-if00
-}
-
 ebb36_flash(){
 	echo -e "\033[1;34m\nStep 4: Cleaning and building Klipper firmware for ebb36.\033[0m"
 	make clean KCONFIG_CONFIG=config.ebb36
@@ -28,6 +16,16 @@ ebb36_flash(){
 	python3 ~/katapult/scripts/flash_can.py -f ~/klipper/ebb36_klipper.bin -u 7923f36815fd
 }
 
+octopus_flash(){
+	echo -e "\033[1;34m\nStep 2: Cleaning and building Klipper firmware for Octopus.\033[0m"
+	make clean KCONFIG_CONFIG=config.octopus
+	make -s KCONFIG_CONFIG=config.octopus -j4  > /dev/null 2>&1
+	mv ~/klipper/out/klipper.bin octopus_klipper.bin
+	echo -e "\033[1;34m\nStep 3: Flashing Klipper to Octopus.\033[0m"
+	python3 ~/katapult/scripts/flashtool.py -i can0 -u 59469bda118c -r
+	sleep 10
+	python3 ~/katapult/scripts/flash_can.py -f ~/klipper/octopus_klipper.bin -d /dev/serial/by-id/stm32h723xx_32001F000F51313531383332-if00
+}
 
 power_cycle(){
 	echo -e "\033[1;34m\nCycling power to PSU and boards.\033[0m"
